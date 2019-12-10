f64d76e3b397004f80510bd45e1cb4b160166012
Gaetano Pessa <g.pessa@goldrepublic.com>
2019-12-10 18:04:00 +0100

    breadcrmub experiment

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,30 +1,68 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
+import { injectIntl } from 'gatsby-plugin-intl';
 
 import Navigation from "../Navigation"
 import Footer from "../Footer"
 import Gdpr from "../Gdpr"
 
-const Layout = ({ children, pageInfo }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children, pageInfo, intl }) => {
+  // debugger
+  return (
+    <StaticQuery
+      // query={graphql`
+      //   query SiteTitleQuery {
+      //     site {
+      //       siteMetadata {
+      //         title
+      //       }
+      //     }
+      //   }
+      // `}
+      query={graphql`
+        query MyQuery {
+          allSitePage(filter: {}) {
+            edges {
+              node {
+                path
+                context {
+                  intl {
+                    language
+                  }
+                }
+              }
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Navigation pageInfo={pageInfo} />
-        <main>{children}</main>
-        <Footer />
-        <Gdpr />
-      </>
-    )}
-  />
-)
+      `}
+      render={data => {
+        function json2array(json) {
+          var result = [];
+          var keys = Object.keys(json);
+          keys.forEach(function (key) {
+            result.push(json[key].node);
+          });
+          return result;
+        }
 
-export default Layout
+        let pages = json2array(data.allSitePage.edges)
+        debugger
+        // pages = pages.map(p => {
+        //   debugger
+        //   return p.node
+        // })
+
+        return (
+          <>
+            <Navigation pageInfo={pageInfo} />
+            <main>{children}</main>
+            <Footer />
+            <Gdpr />
+          </>
+        )
+      }}
+    />
+  )
+}
+
+export default injectIntl(Layout)
