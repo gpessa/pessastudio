e7e9f6f2ff0375187ac94d76425c706a90ddd9b3
Gaetano Pessa <g.pessa@goldrepublic.com>
2019-12-16 17:46:14 +0100

    fix dressagge

--- src/pages/dressage.tsx
+++ src/pages/dressage.tsx
@@ -2,131 +2,129 @@ import { FormattedHTMLMessage, FormattedMessage, injectIntl } from 'gatsby-plugi
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 
-import { Header, Product, Section, Seo, SubSection } from '../components'
+import { Header, Product, Section, Seo } from '../components'
 import { Colors } from '../constants'
 
 const Dressage = ({ intl: { formatMessage } }) => (
   <>
     <Seo title={formatMessage({ id: "META_dressage_title" })} />
 
-    <Section fluid>
-      <Container>
-        <h1>{formatMessage({ id: `NAVIGATION_dressage` })}</h1>
-        <FormattedMessage
-          id="DRESSAGE_intro_1"
-          values={{
-            linktraining: (msg: string) => (
-              <a href="#dressage-training">
-                <strong>{msg}</strong>
-              </a>
-            ),
-            linkolimpic: (msg: string) => (
-              <a href="#dressage-olimpic">
-                <strong>{msg}</strong>
-              </a>
-            ),
-          }}
-        />
-        <p>{formatMessage({ id: "DRESSAGE_intro_2" })}</p>
-      </Container>
+    <Container>
+      <h1>{formatMessage({ id: `NAVIGATION_dressage` })}</h1>
+      <FormattedMessage
+        id="DRESSAGE_intro_1"
+        values={{
+          linktraining: (msg: string) => (
+            <a href="#dressage-training">
+              <strong>{msg}</strong>
+            </a>
+          ),
+          linkolimpic: (msg: string) => (
+            <a href="#dressage-olimpic">
+              <strong>{msg}</strong>
+            </a>
+          ),
+        }}
+      />
+      <p>{formatMessage({ id: "DRESSAGE_intro_2" })}</p>
+    </Container>
 
-      <SubSection id="dressage-training" className="bg-light">
-        <Header as="h2">{formatMessage({ id: `DRESSAGE_training_title` })}</Header>
-        <FormattedHTMLMessage id="DRESSAGE_training_description" />
+    <Section id="dressage-training" className="bg-light">
+      <Header as="h2">{formatMessage({ id: `DRESSAGE_training_title` })}</Header>
+      <FormattedHTMLMessage id="DRESSAGE_training_description" />
 
-        <Row className="mt-5">
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/training-lettera.jpg")}
-              name="Lettera Dressage Light"
-              width={20}
-              height={30}
-              weight={0.5}
-              colors={[Colors.WHITE]}
-              price={20}
-            />
-          </Col>
+      <Row className="mt-5">
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/training-lettera.jpg")}
+            name="Lettera Dressage Light"
+            width={20}
+            height={30}
+            weight={0.5}
+            colors={[Colors.WHITE]}
+            price={20}
+          />
+        </Col>
 
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/training-modulo-dressage.jpg")}
-              name="Modulo dressage"
-              height={28.5}
-              weight={3}
-              colors={[Colors.WHITE]}
-            />
-          </Col>
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/training-modulo-dressage.jpg")}
+            name="Modulo dressage"
+            height={28.5}
+            weight={3}
+            colors={[Colors.WHITE]}
+          />
+        </Col>
 
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/training-rettangolo-dressage-20x60.jpg")}
-              description="Un comodo set composto da 60 moduli e 8 lettere"
-              name="Rettangolo dressage 20x40"
-              height={2000}
-              weight={4000}
-            />
-          </Col>
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/training-rettangolo-dressage-20x60.jpg")}
+            description="Un comodo set composto da 60 moduli e 8 lettere"
+            name="Rettangolo dressage 20x40"
+            height={2000}
+            weight={4000}
+          />
+        </Col>
 
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/training-rettangolo-dressage-20x40.jpg")}
-              description="Un comodo set composto da 80 moduli e 12 lettere"
-              name="Rettangolo dressage 20x60"
-              height={2000}
-              weight={6000}
-            />
-          </Col>
-        </Row>
-      </SubSection>
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/training-rettangolo-dressage-20x40.jpg")}
+            description="Un comodo set composto da 80 moduli e 12 lettere"
+            name="Rettangolo dressage 20x60"
+            height={2000}
+            weight={6000}
+          />
+        </Col>
+      </Row>
+    </Section>
 
-      <SubSection id="dressage-olimpic">
-        <Header as="h2">{formatMessage({ id: `DRESSAGE_olimpic_title` })}</Header>
-        <FormattedHTMLMessage id="DRESSAGE_olimpic_description" />
+    <Section id="dressage-olimpic">
+      <Header as="h2">{formatMessage({ id: `DRESSAGE_olimpic_title` })}</Header>
+      <FormattedHTMLMessage id="DRESSAGE_olimpic_description" />
 
-        <Row className="mt-5">
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/olimpic-lettera.jpg")}
-              name="Lettera Dressage Light"
-              width={39}
-              height={39}
-              weight={2.5}
-              colors={[Colors.WHITE]}
-              price={51}
-            />
-          </Col>
+      <Row className="mt-5">
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/olimpic-lettera.jpg")}
+            name="Lettera Dressage Light"
+            width={39}
+            height={39}
+            weight={2.5}
+            colors={[Colors.WHITE]}
+            price={51}
+          />
+        </Col>
 
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/olimpic-modulo-dressage.jpg")}
-              name="Modulo dressage"
-              height={28.5}
-              weight={3}
-              colors={[Colors.WHITE]}
-            />
-          </Col>
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/olimpic-modulo-dressage.jpg")}
+            name="Modulo dressage"
+            height={28.5}
+            weight={3}
+            colors={[Colors.WHITE]}
+          />
+        </Col>
 
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/olimpic-rettangolo-dressage-20x60.jpg")}
-              description="Un comodo set composto da 60 moduli e 8 letter"
-              name="Rettangolo dressage 20x40"
-              height={2000}
-              weight={4000}
-            />
-          </Col>
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/olimpic-rettangolo-dressage-20x60.jpg")}
+            description="Un comodo set composto da 60 moduli e 8 letter"
+            name="Rettangolo dressage 20x40"
+            height={2000}
+            weight={4000}
+          />
+        </Col>
 
-          <Col md={3}>
-            <Product
-              image={require("../images/product/dressage/olimpic-rettangolo-dressage-20x40.jpg")}
-              description="Un comodo set composto da 80 moduli e 12 letter"
-              name="Rettangolo dressage 20x60"
-              height={2000}
-              weight={6000}
-            />
-          </Col>
-        </Row>
-      </SubSection>
+        <Col md={3}>
+          <Product
+            image={require("../images/product/dressage/olimpic-rettangolo-dressage-20x40.jpg")}
+            description="Un comodo set composto da 80 moduli e 12 letter"
+            name="Rettangolo dressage 20x60"
+            height={2000}
+            weight={6000}
+          />
+        </Col>
+      </Row>
     </Section>
   </>
 )
