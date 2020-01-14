678005171edbdcea312890795268b71c429cb7ed
Gaetano Pessa <g.pessa@goldrepublic.com>
2020-01-14 18:11:05 +0100

    fix locale

--- src/components/LanguageSelector/index.tsx
+++ src/components/LanguageSelector/index.tsx
@@ -1,14 +1,14 @@
-import { changeLocale, IntlContextConsumer } from 'gatsby-plugin-intl';
+import { changeLocale, injectIntl, IntlContextConsumer } from 'gatsby-plugin-intl';
 import * as React from 'react';
 import { NavDropdown } from 'react-bootstrap';
-import { injectIntl } from 'react-intl';
+import { IntlFormatters } from 'react-intl';
 import Flag from 'react-world-flags';
 
 import * as styles from './styles.module.scss';
 
 const LanguageSelectorItem = ({ id }: { id: String }) => <Flag code={id} height={20} className={styles.languageSelectorItem} />
 
-const LanguageSelector = ({ intl: { formatMessage } }) => (
+const LanguageSelector: React.FC<{ intl: IntlFormatters }> = ({ intl: { formatMessage } }) => (
   <IntlContextConsumer>
     {({ languages, language: currentLocale }) => {
       const showLanguageSelector = languages.length > 1;
