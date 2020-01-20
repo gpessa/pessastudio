edc3091eb3485eedb5778110518803efdfc57d8a
Gaetano Pessa <g.pessa@goldrepublic.com>
2020-01-20 16:20:12 +0100

    fix data in cm

--- src/components/Product/Data/index.tsx
+++ src/components/Product/Data/index.tsx
@@ -5,11 +5,13 @@ import { injectIntl } from "gatsby-plugin-intl"
 import * as styles from "../styles.module.scss"
 
 const Data: React.FC<{ intl: IntlFormatters, value?: number, unit?: string, label?: string }> = ({ value, unit, label, intl: { formatMessage, formatNumber } }) => {
-  return value ? (
+  if (!value) return null
+
+  return (
     <div className={styles.data}>
-      <span className={styles.dataLabel}>{formatMessage({ id: label })}:</span> {formatNumber(value)} {unit}.
+      <span className={styles.dataLabel}>{formatMessage({ id: label })}:</span> {formatNumber(value / 10)} {unit}.
     </div>
-  ) : null
+  )
 }
 
 export default injectIntl(Data)
--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -17,21 +17,18 @@ const Product = ({ image, materials, url, name, depth, width, height, diameter, 
   return (
     <Tag to={url} className={`${styles.container} ${className}`}>
       <div className={`${styles.image} ${styles[ratio || Ratio.SQUARE]}`} style={{ backgroundImage: `url(${image})` }}></div>
-
-      <div>
-        <h6 className={styles.title}>{name}</h6>
-        <Description description={description} />
-        <Data value={width} label="PRODUCT__attributo__width" unit="mm" />
-        <Data value={height} label="PRODUCT__attributo__height" unit="mm" />
-        <Data value={length} label="PRODUCT__attributo__length" unit="mm" />
-        <Data value={depth} label="PRODUCT__attributo__depth" unit="mm" />
-        <Data value={diameter} label="PRODUCT__attributo__diameter" unit="mm" />
-        <Data value={thickness} label="PRODUCT__attributo__thickness" unit="mm" />
-        <Data value={weight} label="PRODUCT__attributo__weight" unit="kg" />
-        <MaterialsList materials={materials} />
-        <ColorsList colors={colors} />
-        <Price price={price} />
-      </div>
+      <h6 className={styles.title}>{name}</h6>
+      <Description description={description} />
+      <Data value={width} label="PRODUCT__attributo__width" unit="cm" />
+      <Data value={height} label="PRODUCT__attributo__height" unit="cm" />
+      <Data value={length} label="PRODUCT__attributo__length" unit="cm" />
+      <Data value={depth} label="PRODUCT__attributo__depth" unit="cm" />
+      <Data value={diameter} label="PRODUCT__attributo__diameter" unit="cm" />
+      <Data value={thickness} label="PRODUCT__attributo__thickness" unit="cm" />
+      <Data value={weight} label="PRODUCT__attributo__weight" unit="kg" />
+      <MaterialsList materials={materials} />
+      <ColorsList colors={colors} />
+      <Price price={price} />
     </Tag>
   )
 }
