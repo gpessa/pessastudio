593e0b12f932a361e13d512168a4f0d44c53b9e4
Gaetano Pessa <g.pessa@goldrepublic.com>
2020-01-23 19:15:23 +0100

    some fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,13 @@
-import { injectIntl } from 'gatsby-plugin-intl';
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import { IntlFormatters } from 'react-intl';
+import { injectIntl } from "gatsby-plugin-intl"
+import React from "react"
+import { Col, Row, Container } from "react-bootstrap"
+import { IntlFormatters } from "react-intl"
 
-import { Address, Map, Newsletter, Section } from '@components';
-import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Address, Map, Newsletter, Section } from "@components"
+import { faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import * as styles from './styles.module.scss';
+import * as styles from "./styles.module.scss"
 
 const Footer: React.FC<{ intl: IntlFormatters }> = ({ intl: { formatMessage } }) => (
   <footer className={styles.element}>
@@ -18,20 +18,11 @@ const Footer: React.FC<{ intl: IntlFormatters }> = ({ intl: { formatMessage } })
         <Row>
           <Col md={6} lg={{ span: 4 }} className="mb-5 mb-md-0">
             <h5>{formatMessage({ id: "NAVIGATION__contatti" })}</h5>
-            
-            <Address
-              name="PESSASTUDIO Horse Tecnology srl"
-              country="Italy"
-              postalCode="35044"
-              streetAddress="via Cà Megliadino, 35"
-              addressLocality="Montagnana"
-              addressRegion="Padova"
-              email="annapessa@pessastudio.eu"
-              telephoneMobile="+39 0429 805613"
-            />
 
-            <hr className="border-top"/>
-                      
+            <Address name="PESSASTUDIO Horse Tecnology srl" country="Italy" postalCode="35044" streetAddress="via Cà Megliadino, 35" addressLocality="Montagnana" addressRegion="Padova" email="annapessa@pessastudio.eu" telephoneMobile="+39 0429 805613" />
+
+            <hr className="border-top" />
+
             <div className="mt-2">
               <a target="_blank" href="https://www.facebook.com/Pessastudio-HORSE-Tecnology-1050570271816027/" className={styles.social} aria-label="Facebook" rel="noopener">
                 <FontAwesomeIcon icon={faFacebookSquare} size="lg" />
@@ -50,7 +41,7 @@ const Footer: React.FC<{ intl: IntlFormatters }> = ({ intl: { formatMessage } })
       </Section>
     </div>
 
-    <div className="small text-center text-muted py-3">COD. FISC. e Part: I.V.A. 04743610281 C.C.I.A.A. PD - R.E.A. 414822</div>
+    <Container className="small text-center text-muted py-3">COD. FISC. e Part: I.V.A. 04743610281 C.C.I.A.A. PD - R.E.A. 414822</Container>
   </footer>
 )
 
