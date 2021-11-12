4189a008dec7f866b774ce955e9c1eb46ee125c2
Gaetano Pessa <g.pessa@goldrepublic.com>
2021-11-12 17:17:55 +0100

    Bullshit

--- src/components/SeoElement/index.tsx
+++ src/components/SeoElement/index.tsx
@@ -15,10 +15,6 @@ const Seo: React.FC<Props> = ({ title, description, keywords, meta = [] }) => (
     titleTemplate={`%s | Pessastudio`}
     meta={[
       {
-        name: "charset",
-        content: "UTF-8",
-      },
-      {
         name: `description`,
         content: description,
       },
