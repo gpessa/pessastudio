540843903b4dece7d27720bb9acd229e4a6e9c31
Gaetano Pessa <g.pessa@goldrepublic.com>
2021-11-24 22:22:55 +0100

    Small Refactoring

--- src/components/Data/index.tsx
+++ src/components/Data/index.tsx
@@ -1,7 +1,12 @@
 import { Stack, Typography } from "@mui/material"
 import React from "react"
 
-const Data: React.FC<{ value: string | JSX.Element; label: string | JSX.Element }> = ({ value, label }) => (
+type Props = {
+  label: string
+  value: number | string | JSX.Element | JSX.Element[]
+}
+
+const Data: React.FC<Props> = ({ value, label }) => (
   <Stack direction="row" alignItems="center" mb={0.5}>
     <Typography sx={{ mr: 1 }} gutterBottom={false} variant="caption">
       {label}:
