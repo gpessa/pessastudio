3730a0e067f65a47a0c6688a5a50b36491968c75
Gaetano Pessa <nomeecognome@gmail.com>
2023-04-17 23:22:19 +0200

    Better code

--- src/components/LayoutMui/index.tsx
+++ src/components/LayoutMui/index.tsx
@@ -5,20 +5,18 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import theme from "theme"
 
-const LayoutMui: React.FC = ({ children }) => {
-  return (
-    <>
-      <Helmet>
-        {/* To ensure proper rendering and touch zooming for all devices, add the responsive viewport meta tag to your <head> element. */}
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Helmet>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </>
-  )
-}
+const LayoutMui: React.FC<{ children: JSX.Element }> = ({ children }) => (
+  <>
+    <Helmet>
+      {/* To ensure proper rendering and touch zooming for all devices, add the responsive viewport meta tag to your <head> element. */}
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Helmet>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  </>
+)
 
 export default LayoutMui
