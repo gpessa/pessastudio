dc27abaced89959db418f1007966db9aaf4a0ada
Gaetano Pessa <nomeecognome@gmail.com>
2023-05-03 01:04:12 +0200

    Fix

--- src/pages/dressage.tsx
+++ src/pages/dressage.tsx
@@ -4,6 +4,12 @@ import { getServerSidePropsWithProdcuts } from "utils/lingui";
 export { default } from "modules/Dressage";
 
 export const getServerSideProps = getServerSidePropsWithProdcuts([
-  ProductId.RettangoloDressage_20X60Olympic,
+  ProductId.LetteraDressageTraining,
+  ProductId.ModuloDressageTraining,
+  ProductId.RettangoloDressage_20X60Training,
   ProductId.RettangoloDressage_20X40Training,
+  ProductId.LetteraDressageOlympic,
+  ProductId.ModuloDressageOlympic,
+  ProductId.RettangoloDressage_20X60Olympic,
+  ProductId.RettangoloDressage_20X40Olympic,
 ]);
