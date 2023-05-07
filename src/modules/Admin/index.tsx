902b2f7e18771312b2f767713b127a1aac8cfeb9
Gaetano Pessa <nomeecognome@gmail.com>
2023-05-08 00:42:25 +0200

    Update index.tsx

--- src/modules/Admin/index.tsx
+++ src/modules/Admin/index.tsx
@@ -5,35 +5,34 @@ import { signIn, signOut, useSession } from "next-auth/react";
 export { getServerSideProps } from "utils/getProps";
 
 const Admin = () => {
-  return <div>test</div>;
-  // const { data: session } = useSession();
+  const { data: session } = useSession();
 
-  // return session?.user ? (
-  //   <>
-  //     <Section spacing="small">
-  //       <Grid container alignItems="center" gap={1}>
-  //         <Grid>
-  //           <Avatar
-  //             src={session?.user?.image!}
-  //             alt={(session.user.name || session.user.email)!}
-  //           />
-  //         </Grid>
-  //         <Grid>
-  //           Ciao <strong>{session?.user?.name || session?.user?.email}</strong>{" "}
-  //           (<Link onClick={() => signOut()}>Esci</Link>)
-  //         </Grid>
-  //       </Grid>
-  //     </Section>
-  //     <Section spacing="small">
-  //       <Title title={"Puoi modificare i prezzi"} />
-  //     </Section>
-  //   </>
-  // ) : (
-  //   <Section spacing="small">
-  //     <Title title={"Amministrazione"} />
-  //     <Button onClick={() => signIn()}>Entra</Button>
-  //   </Section>
-  // );
+  return session?.user ? (
+    <>
+      <Section spacing="small">
+        <Grid container alignItems="center" gap={1}>
+          <Grid>
+            <Avatar
+              src={session?.user?.image!}
+              alt={(session.user.name || session.user.email)!}
+            />
+          </Grid>
+          <Grid>
+            Ciao <strong>{session?.user?.name || session?.user?.email}</strong>{" "}
+            (<Link onClick={() => signOut()}>Esci</Link>)
+          </Grid>
+        </Grid>
+      </Section>
+      <Section spacing="small">
+        <Title title={"Puoi modificare i prezzi"} />
+      </Section>
+    </>
+  ) : (
+    <Section spacing="small">
+      <Title title={"Amministrazione"} />
+      <Button onClick={() => signIn()}>Entra</Button>
+    </Section>
+  );
 };
 
 export default Admin;
