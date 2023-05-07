2072fd82fbdafdac3a896ec1bcfe20f974b439aa
Gaetano Pessa <nomeecognome@gmail.com>
2023-05-08 00:39:54 +0200

    mmm

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
-console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET);
-
 export const authOptions = {
   secret: process.env.AUTH_SECRET,
   providers: [
