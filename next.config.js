22d06a0b5aa13968fc0f85df017181eddf20e689
Gaetano Pessa <nomeecognome@gmail.com>
2023-05-27 00:25:53 +0200

    fix babel

--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ module.exports = {
     locales: ['it', 'fr', 'en'],
     defaultLocale: 'en'
   },
-  experimental: {
-    swcPlugins: [
-      ['@lingui/swc-plugin', {
-        // the same options as in .swcrc
-      }],
-    ],
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.po/,
