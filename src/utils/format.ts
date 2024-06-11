cf824e3feafe9dbf60b3f18a8dde2a95da1fe784
Gaetano Pessa <nomeecognome@gmail.com>
2024-06-11 14:04:22 +0200

    fix size

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,11 +1,15 @@
 import { i18n } from "@lingui/core";
 
-export const formatSize = (v: number) =>
-  i18n.number(v, {
+export const formatSize = (v: number, round?: boolean) => {
+  // The feed require integer
+  const value = round ? Math.round(v) : v;
+
+  return i18n.number(v, {
     notation: "standard",
     style: "unit",
     unit: "centimeter",
   });
+};
 
 export const formatSizeMeter = (v: number) =>
   i18n.number(v / 100, {
