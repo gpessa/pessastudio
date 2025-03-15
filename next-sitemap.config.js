cf5661a3aea0e5cbb486075c91ba37b7f0b8e0b7
Gaetano Pessa <nomeecognome@gmail.com>
2025-03-15 23:32:54 +0100

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    exclude: ['/404', '/500', '/admin'],
+    exclude: ['/404', '/500'],
     generateRobotsTxt: true,
     siteUrl: process.env.NEXT_PUBLIC_WEBISTE_URL
 }
