8cea00da9a8066d1603a87a13d7118ea135ec04f
Gaetano Pessa <nomeecognome@gmail.com>
2025-05-28 10:40:32 +0200

    added speed insight & analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { GOOGLE_ANALYTICS, IS_PRODUCTION } from "@/constants";
 import theme from "@/theme";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { PropsWithChildren } from "react";
 
 export default async function RootLayout({
@@ -10,7 +13,9 @@ export default async function RootLayout({
     <html>
       <body>
         <AppRouterCacheProvider>
+          {IS_PRODUCTION && <GoogleAnalytics gaId={GOOGLE_ANALYTICS} />}
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <SpeedInsights />
         </AppRouterCacheProvider>
       </body>
     </html>
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+// TODO: Replace with your actual Google Analytics ID
+export const GOOGLE_ANALYTICS = "G-XXXXXXXXXX";
+
+export const IS_PRODUCTION = !!process && process.env.NODE_ENV === "production";
