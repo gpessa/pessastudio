4eb0406c872f66c2756df02711f4bcb3f732fc7a
Gaetano Pessa <nomeecognome@gmail.com>
2025-11-20 16:48:33 +0100

    update deps

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,9 +3,10 @@ import { i18nRouter } from "next-i18n-router";
 
 import i18nConfig from "../i18nConfig";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   return i18nRouter(request, i18nConfig);
 }
+
 export const config = {
   matcher: [
     /*
